d5444e54ab1dc64a6aef3c10f1978bfb5fc19c56
yashdesai <yddc902@live.com>
2021-04-19 08:55:38 +0530

    p added

--- src/index.php
+++ src/index.php
@@ -20,6 +20,7 @@
     <div class="flex flex-col items-center space-y-2 text-center px-6">
       <h1 class="font-bold text-3xl text-gray-800 tracking-wide">All in one Expense Tracker</h1>
       <p class="leading-relaxed tracking-wide">Save today for tomorrow for you and your family. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+      <p class="leading-relaxed tracking-wide">Save today for tomorrow for you and your family. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
     </div>
     <div class="flex justify-center py-12">
       <a href="" class="bg-indigo-500 font-semibold tracking-wide text-indigo-50 uppercase px-6 py-2 rounded text-lg hover:bg-indigo-600">Start now</a>
