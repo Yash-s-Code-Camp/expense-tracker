bc8a904b653806fa49b298a06db1fedb8111088a
yashdesai <yddc902@live.com>
2021-04-22 07:47:19 +0530

    signup page, logo

--- src/layouts/top.php
+++ src/layouts/top.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Expense Tracker</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
@@ -13,15 +13,18 @@
   <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a href="./index.php" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 5a1 1 0 100 2h1a2 2 0 011.732 1H7a1 1 0 100 2h2.732A2 2 0 018 11H7a1 1 0 00-.707 1.707l3 3a1 1 0 001.414-1.414l-1.483-1.484A4.008 4.008 0 0011.874 10H13a1 1 0 100-2h-1.126a3.976 3.976 0 00-.41-1H13a1 1 0 100-2H7z" clip-rule="evenodd" />
         </svg>
-        <span class="ml-3 text-xl">Tailblocks</span>
+        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+        </svg> -->
+        <span class="ml-3 text-xl">Expense Tracker</span>
       </a>
       <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center space-x-5">
         <a href="./index.php" class="flex tems-center  py-1 px-2 rounded hover:bg-indigo-500 hover:text-indigo-50">Home</a>
         <a href="./about.php" class="flex items-center py-1 px-2 rounded hover:bg-indigo-500 hover:text-indigo-50">About</a>
-        <a href="./contact.php" class="flex tems-center  py-1 px-2 rounded hover:bg-indigo-500 hover:text-indigo-50">contact Us</a>
+        <a href="./contact.php" class="flex tems-center  py-1 px-2 rounded hover:bg-indigo-500 hover:text-indigo-50">Contact Us</a>
         <a href="./login.php" class="inline-flex items-center bg-indigo-500 text-indigo-50 border-0 py-1 px-3 focus:outline-none hover:animate-pulse hover:text-indigo-50 rounded text-base mt-4 md:mt-0 uppercase">Start now
         </a>
       </nav>
---
+++ src/signup.php
@@ -0,0 +1,26 @@
+<?php include "./layouts/top.php"; ?>
+<div class=" flex flex-col px-24 lg:px-64  py-24 justify-center items-center space-y-4">
+  <div class="lg:w-2/6  bg-gray-100 rounded-lg p-8 flex flex-col  mt-10 md:mt-0">
+    <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Signup</h2>
+    <div class="relative mb-4">
+      <label for="text" class="leading-7 text-sm text-gray-600">Full Name</label>
+      <input type="text" id="fullname" name="fullname" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+    </div>
+    <div class="relative mb-4">
+      <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+      <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+    </div>
+    <div class="relative mb-4">
+      <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
+      <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+    </div>
+    <div class="relative mb-4">
+      <label for="password" class="leading-7 text-sm text-gray-600">Re-type Password</label>
+      <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+    </div>
+    <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Signup</button>
+    
+    <a href="./signup.php" class="text-sm text-indigo-500 mt-3 underline hover:text-indigo-400">Already have an account?</a>
+  </div>
+</div>
+<?php include "./layouts/bottom.php"; ?>
