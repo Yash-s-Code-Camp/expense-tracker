bdd3a9fe7a12d369694e034720026a5a7447c972
mscomptuts <manalisamani3008@gmail.com>
2021-04-28 09:10:50 +0530

    responsive navbar

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const hum_icon = document.querySelector('#hamburger')
+const close_icon = document.querySelector('#close')
+
+hum_icon.addEventListener('click', function()
+{
+    close_icon.classList.remove('hidden')
+    close_icon.classList.add('block')
+
+    hum_icon.classList.remove('block')
+    hum_icon.classList.add('hidden')
+
+    console.log('hamburger click')
+})
+close_icon.addEventListener('click', function()
+{
+    close_icon.classList.remove('block')
+    close_icon.classList.add('hidden')
+
+    hum_icon.classList.remove('hidden')
+    hum_icon.classList.add('block')
+
+    console.log('close click')
+})
