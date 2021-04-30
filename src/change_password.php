c364423dd6d4db8ff0c56f1ee579746eeef2b7ad
Nikunj Thakor <38680557+nnikunjt@users.noreply.github.com>
2021-04-30 22:36:25 +0530

    update password message added

--- src/change_password.php
+++ src/change_password.php
@@ -53,7 +53,7 @@
       <button type="submit" name="change_pwd" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Change Password</button>
 
       <div id="alert" class=" mt-4  px-4 py-3 rounded relative <?= (($error == 0) ? "bg-green-100  border-green-400 text-green-700" : "bg-red-100  border-red-400 text-red-700"); ?> <?= (isset($error) ? '' : 'hidden' ) ?> <?= (isset($error) ? '' : 'hidden' ) ?> " role="alert">
-        <strong class="font-semibold"><?= ($error == 0)? "Password updated succesfully!" : "Passwords are not matching!" ?></strong>
+        <strong class="font-semibold"><?= ($error == 0)? "Password has been updated succesfully!" : "Passwords are not matching!" ?></strong>
         
       </div>
 
