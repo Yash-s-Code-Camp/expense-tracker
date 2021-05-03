0759baff7e3bb9e11b212c86b3e475e68d9263ca
yashdesai <yddc902@live.com>
2021-05-03 11:10:55 +0530

    dashboard

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,31 +1,43 @@
 <?php
 
-    session_start();
-    if (!isset($_SESSION['email'])) {
-        header("location:login.php");
-    }
-    else{
-        echo "<h1>".$_SESSION['email']."</h1>";
-    }
+session_start();
+if (!isset($_SESSION['email'])) {
+    header("location:login.php");
+} else {
+    echo "<h1>" . $_SESSION['email'] . "</h1>";
+}
 
-
-    if (isset($_POST['logout'])) {
-        session_destroy();
-        header("location:index.php");
-    }
+if (isset($_POST['logout'])) {
+    session_destroy();
+    header("location:index.php");
+}
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="post">
-        <button type="submit" name="logout">Logout</button>
-    </form>
-</body>
-</html>
+<div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4 text-center">
+        <div class="p-4 sm:w-1/4 w-1/2">
+            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">30K</h2>
+            <p class="leading-relaxed">Income</p>
+        </div>
+        <div class="p-4 sm:w-1/4 w-1/2">
+            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">3K</h2>
+            <p class="leading-relaxed">Household</p>
+        </div>
+        <div class="p-4 sm:w-1/4 w-1/2">
+            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">5K</h2>
+            <p class="leading-relaxed">Food</p>
+        </div>
+        <div class="p-4 sm:w-1/4 w-1/2">
+            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2K</h2>
+            <p class="leading-relaxed">Electricals</p>
+        </div>
+        <div class="p-4 sm:w-1/4 w-1/2">
+            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">15K</h2>
+            <p class="leading-relaxed">Bills</p>
+        </div>
+        <div class="p-4 sm:w-1/4 w-1/2">
+            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">25K</h2>
+            <p class="leading-relaxed">Total Expense</p>
+        </div>
+    </div>
+</div>
