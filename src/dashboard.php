b1705d21c145f056530c722703625e0b94a0fc6e
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-05-03 20:07:10 +0530

    Dashboard Card design added

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,10 +1,10 @@
+<?php include "./layouts/top.php"; ?>
+
 <?php
 
 session_start();
 if (!isset($_SESSION['email'])) {
     header("location:login.php");
-} else {
-    echo "<h1>" . $_SESSION['email'] . "</h1>";
 }
 
 if (isset($_POST['logout'])) {
@@ -13,31 +13,69 @@ if (isset($_POST['logout'])) {
 }
 
 ?>
+
 <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4 text-center">
-        <div class="p-4 sm:w-1/4 w-1/2">
-            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">30K</h2>
-            <p class="leading-relaxed">Income</p>
+    <div class="flex flex-wrap text-center justify-center m-auto">
+
+        <div class="sm:w-1/2 xl:w-1/4 md:w-1/3 m-5  w-full bg-gray-50 rounded shadow-xl text-center">
+            <div class="flex flex-col items-center w-fullp-2">
+                <div class="flex-1 text-center md:text-center text-gray-600 p-5">
+                    <h3 class="text-4xl py-2">30K</h3>
+                </div>
+                <div class="flex-1 text-center text-gray-600 rounded bg-indigo-400 w-full">
+                    <h3 class="font-semi-bold text-2xl py-1 text-gray-50">Income</h3>
+                </div>
+            </div>
         </div>
-        <div class="p-4 sm:w-1/4 w-1/2">
-            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">3K</h2>
-            <p class="leading-relaxed">Household</p>
+        <div class="sm:w-1/2 xl:w-1/4 md:w-1/3 m-5  w-full bg-gray-50 rounded shadow-xl text-center">
+            <div class="flex flex-col items-center w-fullp-2">
+                <div class="flex-1 text-center md:text-center text-gray-600 p-5">
+                    <h3 class="text-4xl py-2">3K</h3>
+                </div>
+                <div class="flex-1 text-center text-gray-600 rounded bg-indigo-400 w-full">
+                    <h3 class="font-semi-bold text-2xl py-1 text-gray-50">Household</h3>
+                </div>
+            </div>
         </div>
-        <div class="p-4 sm:w-1/4 w-1/2">
-            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">5K</h2>
-            <p class="leading-relaxed">Food</p>
+        <div class="sm:w-1/2 xl:w-1/4 md:w-1/3 m-5  w-full bg-gray-50 rounded shadow-xl text-center">
+            <div class="flex flex-col items-center w-fullp-2">
+                <div class="flex-1 text-center md:text-center text-gray-600 p-5">
+                    <h3 class="text-4xl py-2">5K</h3>
+                </div>
+                <div class="flex-1 text-center text-gray-600 rounded bg-indigo-400 w-full">
+                    <h3 class="font-semi-bold text-2xl py-1 text-gray-50">Food</h3>
+                </div>
+            </div>
         </div>
-        <div class="p-4 sm:w-1/4 w-1/2">
-            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2K</h2>
-            <p class="leading-relaxed">Electricals</p>
+        <div class="sm:w-1/2 xl:w-1/4 md:w-1/3 m-5  w-full bg-gray-50 rounded shadow-xl text-center">
+            <div class="flex flex-col items-center w-fullp-2">
+                <div class="flex-1 text-center md:text-center text-gray-600 p-5">
+                    <h3 class="text-4xl py-2">2K</h3>
+                </div>
+                <div class="flex-1 text-center text-gray-600 rounded bg-indigo-400 w-full">
+                    <h3 class="font-semi-bold text-2xl py-1 text-gray-50">Electricals</h3>
+                </div>
+            </div>
         </div>
-        <div class="p-4 sm:w-1/4 w-1/2">
-            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">15K</h2>
-            <p class="leading-relaxed">Bills</p>
+        <div class="sm:w-1/2 xl:w-1/4 md:w-1/3 m-5  w-full bg-gray-50 rounded shadow-xl text-center">
+            <div class="flex flex-col items-center w-fullp-2">
+                <div class="flex-1 text-center md:text-center text-gray-600 p-5">
+                    <h3 class="text-4xl py-2">15K</h3>
+                </div>
+                <div class="flex-1 text-center text-gray-600 rounded bg-indigo-400 w-full">
+                    <h3 class="font-semi-bold text-2xl py-1 text-gray-50">Bills</h3>
+                </div>
+            </div>
         </div>
-        <div class="p-4 sm:w-1/4 w-1/2">
-            <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">25K</h2>
-            <p class="leading-relaxed">Total Expense</p>
+        <div class="sm:w-1/2 xl:w-1/4 md:w-1/3 m-5  w-full bg-gray-50 rounded shadow-xl text-center">
+            <div class="flex flex-col items-center w-fullp-2">
+                <div class="flex-1 text-center md:text-center text-gray-600 p-5">
+                    <h3 class="text-4xl py-2">25K</h3>
+                </div>
+                <div class="flex-1 text-center text-gray-600 rounded bg-indigo-400 w-full">
+                    <h3 class="font-semi-bold text-2xl py-1 text-gray-50">Total Expense</h3>
+                </div>
+            </div>
         </div>
     </div>
 </div>
