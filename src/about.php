7e761bdf0090cc6f0d857a5d7cb1d60d8a55e6e4
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-07-11 18:50:32 +0530

    About and Contact added

---
+++ src/about.php
@@ -0,0 +1,23 @@
+<?php
+include "./layouts/top.php";
+
+?>
+
+<div class="m-auto w-full">
+    <h1 class="text-4xl text-center mb-10">
+        About Us Page
+    </h1>
+    <p class="tracking-wider text-lg text-center">
+        This is demo line......
+    </p>
+    <p class="tracking-wider text-lg text-center">
+        This is demo line......
+    </p>
+    <p class="tracking-wider text-lg text-center">
+        This is demo line......
+    </p>
+</div>
+
+<?php
+include "./layouts/bottom.php";
+?>
--- src/layouts/top.php
+++ src/layouts/top.php
@@ -9,6 +9,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Expense Tracker</title>
   <link rel="stylesheet" href="style.css">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
   <script src="main.js" defer></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js" integrity="sha256-xKeoJ50pzbUGkpQxDYHD7o7hxe0LaOGeguUidbq6vis=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
